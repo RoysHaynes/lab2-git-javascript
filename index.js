87635fa1e3177889853264d05e503b84f0d010ab
roysh <rhaynes@sandiego.edu>
2025-09-14 18:54:54 -0700

    feature: Scaffold strings module and add console tests to main file

--- index.js
+++ index.js
@@ -6,6 +6,8 @@
 
 // Import math functions
 import * as math from './math.js';
+import * as strings from './strings.js';
+import {isPalindrome} from "./strings.js";
 
 console.log("====Testing Math Function===");
 console.log('add(5,3)=',math.add(5,3)); //should be 8
@@ -15,3 +17,10 @@ console.log('divide(20,4)=',math.divide(20,4)); //should be 5
 console.log('power(2,8)=',math.power(2,8));// should be 256
 console.log('sqrt(16)=',math.sqrt(16));// should be 4
 console.log('factorial(5)=',math.factorial(5));// should be 120
+
+console.log('\n=== Testing String module===');
+console.log('reverse("hello")=', strings.reverse("hello"));
+console.log('capitalize("javascript")=', strings.capitalize("javascript"));
+console.log('countVowels("hello world")=', strings.countVowels("hello world"));
+console.log('isPalindrome("racecar")=', isPalindrome("racecar"));
+console.log('isPalindrome("hello")=', isPalindrome("hello"));
